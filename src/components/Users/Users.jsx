9504571f6125278f280d7628efff6b34ce4a4a92
Barry <barrymurphy_9@hotmail.com>
2024-03-16 13:37:11 +0000

    loading component added

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,15 +1,20 @@
 import { useState, useEffect } from "react";
 import { getUsers } from "../../api";
 import UserCard from "../UserCard/UserCard";
+import Loading from "../Loading/Loading";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     getUsers().then((usersFromApi) => {
       setUsers(usersFromApi);
+      setIsLoading(false);
     });
   }, []);
+
+  if (isLoading) return <Loading />;
 
   return (
     <div>
