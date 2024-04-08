4da9ad3cce456c7c2b359b9be3f1779ea00ac0ca
Barry <barrymurphy_9@hotmail.com>
2024-04-08 15:19:07 +0100

    article cards whole card made clickable and date format changed to timeAgo

--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -1,18 +1,25 @@
 import "./ArticleCard.css";
 import { Link } from "react-router-dom";
+import { formatDistanceToNow } from "date-fns";
+
+
 const ArticleCard = ({ article }) => {
+
+  const timeAgo = formatDistanceToNow(new Date(article.created_at), { addSuffix: true });
   return (
+    <Link to={`/articles/${article.article_id}`}>
     <div className="articleCard">
       <h3>
-        <Link to={`/articles/${article.article_id}`}>{article.title}</Link>
+        {article.title}
       </h3>
-      <p>{article.topic}</p>
-      <p>{article.author}</p>
-      <p>{article.created_at}</p>
-      <img src={article.article_img_url} alt="" />
-      <p>Votes: {article.votes}</p>
-      <p>Comments: {article.comment_count}</p>
+      <p><strong>Topic:</strong> {article.topic}</p>
+      <p><strong>Author:</strong> {article.author}</p>
+      <img className="articleImage" src={article.article_img_url} alt="" />
+      <p><strong>Votes:</strong> {article.votes}</p>
+      <p><strong>Comments:</strong> {article.comment_count}</p>
+      <p>{timeAgo}</p>
     </div>
+    </Link>
   );
 };
 
