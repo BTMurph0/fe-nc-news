3d70fd9a1a78e99e77d07055b9e950515d1f8305
Barry <barrymurphy_9@hotmail.com>
2024-05-09 15:55:50 +0100

    made article page more responsive

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -18,7 +18,7 @@ const CommentList = ({setCommentCount}) => {
   }, []);
 
   return (
-    <article>
+    <div>
       {login.username != "guest" && <CommentAdder setComments={setComments} setCommentCount={setCommentCount}/>}
       <ul>
         {comments.map((comment, i) => {
@@ -27,7 +27,7 @@ const CommentList = ({setCommentCount}) => {
           );
         })}
       </ul>
-    </article>
+    </div>
   );
 };
 
