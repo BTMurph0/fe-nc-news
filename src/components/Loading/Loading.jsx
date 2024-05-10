1e38aa7fec37c352526f0c3b3edc20e71db2b077
Barry <barrymurphy_9@hotmail.com>
2024-05-10 15:43:50 +0100

    removed trailing semicolon from loading spinner

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -3,10 +3,9 @@ import "./Loading.css";
 const Loading = () => {
   return (
     <>
-    <div className="loader"></div>;
+      <div className="loader"></div>
     </>
-  )
-
+  );
 };
 
 export default Loading;
